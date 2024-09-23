5c7359c10ce6badd3739fe34b1ce3e8f1b7613da
Zohirul <Zohirul@TVLLAP14>
2024-09-23 11:35:09 +0600

    Add project files.

---
+++ Responsiv_Dashborad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Responsiv_Dashborad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
